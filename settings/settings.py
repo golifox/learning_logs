7db2b93091f044435d8bd290198ba613268dc03b
David <golifox@yandex.ru>
2022-09-27 11:03:41 +0300

    fix admin looks

--- settings/settings.py
+++ settings/settings.py
@@ -21,8 +21,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'my-secret-key'
 SECRET_KEY = env('SECRET_KEY')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
