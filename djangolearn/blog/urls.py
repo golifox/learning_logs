c2a3aeffcc08c9e4fbdc73ca173de30df3fc5730
David <golifox@yandex.ru>
2022-10-11 06:56:41 +0300

    add static

---
+++ djangolearn/blog/urls.py
@@ -0,0 +1,26 @@
+"""Defines URL patterns for blog."""
+
+from django.urls import path
+
+from . import views
+
+app_name= 'blog'
+urlpatterns = [
+    # Home page.
+    path('', views.index, name='index'),
+
+    # Page that showing all topics.
+    path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    # Page for adding a new Topic.
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # Page for adding a new entry.
+    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
+
+    # Page for editing entries
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry')
+]   
---
+++ djangolearn/blog/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
+
+def index(request):
+    """The home page for Blog."""
+    return render(request, 'blog/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'blog/topics.html', context)
+
+def topic(request, topic_id):
+    """Show one selected topic."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'blog/topic.html', context) 
+
+def new_topic(request):
+    """Add new topic."""
+    if request.method != 'POST':
+        # No data submitted. Create blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:topics')
+
+    # Display blank or invalid form.
+    context = {'form': form}
+    return render(request, 'blog/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+ 
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('blog:topic', topic_id=topic_id)
+            
+    # Display a blank or invalid form.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'blog/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'blog/edit_entry.html', context)
